aa778c97915c6a10a5f3e0c07768868f78c3778a
smaecrof <smaecrof@iu.edu>
2020-08-14 09:30:15 -0500

    TwoSum Problem completed
    This was a simple problem that I went back over to do again to make sure
    I really understood the solution. Leetcode is a valuable resource that I
    need to continually use in order to better my programming problem
    solving skills.
    
    08/14/2020 Coronavirus Learning

--- LeetCodeProblems/src/TwoSum.java
+++ LeetCodeProblems/src/TwoSum.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
+
+        int[] twoSumReturn = twoSum(nums,target);
+        for(int i:twoSumReturn){
+            System.out.print(i + ", ");
+        }
+    }
+    public static int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        int complement = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
 }
