04f082b891e08f1287f46956eda4e444a30af4d6
smaecrof <smaecrof@iu.edu>
2020-08-15 15:44:19 -0500

    Thread Mania
    I just learned about how to create multiple threads so that the UI
    thread is not bogged down by slow processes. I of course had to make a
    new Runnable object which I simply used a Thread.sleep(10000) operation
    to handle like a long process. What was interesting was the idea that I
    had to use Platform.runLater() with a new runnable object within this
    other runnable object in order to update a component in the UI. I
    believe the reason I have to do this is so that the UI stays thread
    safe. We wouldn't want one thread to interact with another threads
    actions while they are running.
    
    08/15/2020 Coding Day #13

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -15,6 +17,8 @@ public class Controller {
     private Button byeButton;
     @FXML
     private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
 
     @FXML
     public void initialize(){
@@ -29,6 +33,27 @@ public class Controller {
         } else if (e.getSource().equals(byeButton)){
             System.out.println("Bye, " + nameField.getText());
         }
+
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    String s = Platform.isFxApplicationThread() ? "UI Thread":"Background Thread";
+                    System.out.println("I'm going to sleep on the " + s);
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s = Platform.isFxApplicationThread() ? "UI Thread":"Background Thread";
+                            System.out.println("I'm updating the label on the  " + s);
+                            ourLabel.setText("Runnable Completed!");
+                        }
+                    });
+                } catch(InterruptedException event){
+                }
+            }
+        };
+        new Thread(task).start(); // Creating separate thread outside of UI thread
 
         if(ourCheckBox.isSelected()){
             nameField.clear();
