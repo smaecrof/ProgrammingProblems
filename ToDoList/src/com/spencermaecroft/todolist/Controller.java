752574bb584522277f53cfdf862390e525f12b6b
smaecrof <smaecrof@iu.edu>
2020-08-16 11:36:09 -0500

    Before Singleton Addition
    I am at the stage in my program where I have everything working as I'd
    want with mock data but now I need to switch the program to actually
    store and then load data into the application. I will start with
    creating a singleton class to store data and then I will build off of
    that design. I wanted to make this commit before moving onto this larger
    structural change so that if I want to restart the new build, I can
    start with this commit.
    
    08/16/2020 Coding Day #14

--- ToDoList/src/com/spencermaecroft/todolist/Controller.java
+++ ToDoList/src/com/spencermaecroft/todolist/Controller.java
@@ -1,14 +1,31 @@
 package com.spencermaecroft.todolist;
 
 import dataModel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
     private List<TodoItem> todoItems;
+
+    @FXML
+    private ListView<TodoItem>todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    @FXML
+    private Label deadlineLabel;
 
     public void initialize(){
         TodoItem item1 = new TodoItem("Exercise", "Lift shoulders and biceps",
@@ -16,7 +33,7 @@ public class Controller {
         TodoItem item2 = new TodoItem("Program", "Study the Java Programming Language",
                 LocalDate.of(2020, Month.AUGUST,16));
         TodoItem item3 = new TodoItem("Make a healthy lunch", "Make some chicken and rice",
-                LocalDate.of(2020, Month.AUGUST,16));
+                LocalDate.of(2020, Month.SEPTEMBER,20));
         TodoItem item4 = new TodoItem("Program", "Work on some leetcode problems for some alg. practice",
                 LocalDate.of(2020, Month.AUGUST,16));
         TodoItem item5 = new TodoItem("Go for a run", "Go for a run in the Portage Running trails",
@@ -28,5 +45,28 @@ public class Controller {
         todoItems.add(item3);
         todoItems.add(item4);
         todoItems.add(item5);
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldItem, TodoItem newValue) {
+                if(newValue != null){
+                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
+                }
+            }
+        });
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void handleClickListView(){
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        itemDetailsTextArea.setText(item.getDetails());
+        deadlineLabel.setText(item.getDeadline().toString());
     }
 }
