eb278b99df7fdb7340c812f476320b146f516a36
smaecrof <smaecrof@iu.edu>
2020-08-19 13:04:47 -0500

    File & Directory Input JavaFX
    I learned how to use file and directory input operations in an
    application. I had wondered how to do this for a while now and I finally
    checked this topic off of my todo list. I also learned a little about
    how to create webpage links in my application. I cannot wait until I
    learn how to create cross-platform applications.
    
    08/19/2020 Day# 16

--- JavaFXApplication/src/sample/Controller.java
+++ JavaFXApplication/src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -39,6 +40,13 @@ public class Controller {
     @FXML
     public void handleClick(){
         FileChooser chooser = new FileChooser();
+        chooser.setTitle("Save Application File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text", "*.txt"),
+                new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+        );
+//        DirectoryChooser dirChooser = new DirectoryChooser();
         File file = chooser.showOpenDialog(gridpane.getScene().getWindow());
 
         if(file != null){
