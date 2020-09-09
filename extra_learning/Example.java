48dae3a07ffeda5aeaa0dbf46b4d492529abf3ee
smaecrof <smaecrof@iu.edu>
2020-09-09 17:02:58 -0500

    Addition to Example.java
    I've been checking out some of Java's new features such as local
    variable type inference which has been a cool experience. I've really
    started to dive into the weeds when it comes to this language.
    
    09/09/2020

--- extra_learning/Example.java
+++ extra_learning/Example.java
@@ -5,5 +5,17 @@
 class Example{
 	public static void main(String[] args){
 		System.out.println("This is a simple Java program");
+
+		int array[] = {2,4,6,8};
+		int array2[] = new int[4];
+
+		for(var i = 0; i < 4; i++){
+			array2[i] = (i+1) * 2; 
+		}
+
+		for(int i = 0; i < 4; i++){
+			System.out.println("array[" + i + "] = " + array[i]); 
+			System.out.println("array2[" + i + "] = " +  array2[i]);
+		}
 	}
 }
