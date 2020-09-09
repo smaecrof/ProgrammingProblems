fcda12d2b43a9498b41b3dd694b173db4164b80c
smaecrof <smaecrof@iu.edu>
2020-09-09 13:33:00 -0500

    Misc file commit
    Multitude of files that need to be committed
    
    09/09/2020

---
+++ AnonymousClassCuriosity/src/com/spencermaecroft/Main.java
@@ -0,0 +1,18 @@
+package com.spencermaecroft;
+
+import javax.naming.MalformedLinkException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        GreetingRobot jarvis = new GreetingRobot();
+////        jarvis.sayHello();
+//
+//        jarvis.sayGreeting("Spencer");
+//
+//        jarvis.greetInSpanish("Elise");
+
+        Person p1 = Person("Jake", Person.SEX.MALE, 21);
+    }
+}
