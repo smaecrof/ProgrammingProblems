6f07314d648898031404a07173a53ba683b9686d
smaecrof <smaecrof@iu.edu>
2020-09-09 20:06:32 -0500

    MaxSubArray
    This was a decently hard algorithms problem where I had to find the
    largest sub array within a very long array. The challenge did not allow
    for brute force approaches and we had to stay at constant time. I will
    review this problem more in the future as it was difficult to break up
    the problem as a whole.
    
    09/09/2020

--- LeetCodeProblems/src/MaxSubarray.java
+++ LeetCodeProblems/src/MaxSubarray.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MaxSubarray {
+public class MaxSubarray {
+
+    public static void main(String[] args) {
+        int input[] = {-2,1,-3,4,-1,2,1,-5,4};
+        int input2[] = {-2147483647};
+        System.out.println(maxSubArray(input));
+        System.out.println(maxSubArray(input2));
+    }
+
+    public static int maxSubArray(int[] nums){
+        int n = nums.length;
+        int[] dp = new int[n]; // dp[i] means the maximum subarray ending with A[i]
+        dp[0] = nums[0];
+        int max = dp[0];
+
+        for(int i = 1; i < n; i++){
+            dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1]:0);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
 }
