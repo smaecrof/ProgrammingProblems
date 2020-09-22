a4d7f8152fd9a31f56dd399c72b2728fa5c25881
smaecrof <smaecrof@iu.edu>
2020-09-21 19:02:47 -0500

    Created variable interface
    I found it interesting how many use cases there are for interfaces such
    as a variable interface. I had a few constant variables in the interface
    and with no implementation (methods).
    
    09/21/2020

---
+++ InterfaceVariables/src/Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+    public static void main(String[] args) {
+        Question q = new Question();
+
+        AskMe.answer(q.ask());
+        AskMe.answer(q.ask());
+        AskMe.answer(q.ask());
+        AskMe.answer(q.ask());
+    }
+}
