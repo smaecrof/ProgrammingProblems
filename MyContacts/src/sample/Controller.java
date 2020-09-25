21febdff9e4832f219164c5f707a4c5f67853975
smaecrof <smaecrof@iu.edu>
2020-09-25 08:41:29 -0500

    Edit Feature Added
    The edit feature was added to the contact application. When you select
    a contact and then click "Edit..". This will prompt a dialog box with
    the contacts data present to which you can modify and then save.
    - Now I am moving onto a "Delete.." button that will delete a contact
    only after an alert has asked if they want to actually delete
    information.
    
    09/25/2020

--- MyContacts/src/sample/Controller.java
+++ MyContacts/src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TableView;
@@ -37,7 +38,7 @@ public class Controller {
         dialog.initOwner(mainPanel.getScene().getWindow());
         dialog.setTitle("Add New Contact");
         FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("contactDialog.fxml"));
+        fxmlLoader.setLocation(getClass().getResource("contactdialog.fxml"));
 
         try {
             dialog.getDialogPane().setContent((fxmlLoader.load()));
@@ -63,6 +64,49 @@ public class Controller {
             - Try to implement this differently so that only new data is added maybe to a buffer
             which is then periodically added to main storage...
              */
+            data.saveContacts();
+        }
+    }
+
+    @FXML
+    public void showEditContactDialog(){
+        Contact selectedContact = contactsTable.getSelectionModel().getSelectedItem();
+        if(selectedContact == null){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("No Contact selected");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select the contact you want to edit");
+            alert.showAndWait();
+            return;
+        }
+
+        Dialog<ButtonType> dialog = new Dialog<ButtonType>();
+        dialog.initOwner(mainPanel.getScene().getWindow());
+        dialog.setTitle("Edit Contact");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("contactdialog.fxml"));
+
+        try {
+            dialog.getDialogPane().setContent((fxmlLoader.load()));
+        } catch (IOException e){
+            System.out.println("Couldn't load the dialog");
+            return;
+        }
+        catch(IllegalStateException e){
+            System.out.println("IllegalStateException ");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        ContactController contactController = fxmlLoader.getController();
+        contactController.editContact(selectedContact);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK){
+            contactController.updateContact(selectedContact);
             data.saveContacts();
         }
     }
