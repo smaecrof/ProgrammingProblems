a1a34399bc038faec7756135f8409848deb042e4
smaecrof <smaecrof@iu.edu>
2020-09-26 12:22:26 -0500

    Basic Application start
    So far in the applicaiton I have set up a foundation with my main fxml
    file, Controller java file and a css file. I have started to arrange
    items in the applicaiton and do some default styling just to get a
    better idea of how the layout will work and what I'm actually shooting
    for. I've added the default buttons that will be the main naviagtion for
    the application. I am going to finish a few css attributes and then move
    onto linking actions and starting to get individual scenes built.
    
    09/26/2020

--- StudyCycle/src/sample/Main.java
+++ StudyCycle/src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("StudyCycle");
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
