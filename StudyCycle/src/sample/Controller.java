eec78e8eaa70a6c0efb38a85315164633b82c09e
smaecrof <smaecrof@iu.edu>
2020-09-26 18:54:39 -0500

    Nightly Commit
    I am cashing in on the project for the day. I worked really hard on the
    project while also reading a couple hundred pages of some other material
    which made for a productive day. I am about to study data structures and
    algorithms for a few hours tonight and maybe crack into my Operating
    Systems material for a while as well. I will pick up on the project
    tomorrow.
    
    09/26/2020

--- StudyCycle/src/sample/Controller.java
+++ StudyCycle/src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
+import javax.net.ssl.HostnameVerifier;
 import java.util.IdentityHashMap;
 
 /**
@@ -14,7 +15,7 @@ public class Controller {
     private static final String HOVERED_BUTTON_STYLE = "-fx-background-color: rgb(30, 63, 32)";
     private static final String IDLE_BUTTON_STYLE = "-fx-background-color: rgb(52,88,48)";
     @FXML
-    private Button decksBtn, addBtn, browseBtn, statsBtn;
+    private Button decksBtn, addBtn, browseBtn, statsBtn, currentBtnClicked;
 
     @FXML
     private BorderPane mainPanel;
@@ -22,33 +23,50 @@ public class Controller {
     public void initialize(){
         decksBtn.setStyle(HOVERED_BUTTON_STYLE);
 
-        decksBtn.setOnMouseEntered(event -> {
+
+        decksBtn.setOnMousePressed(event ->{
             decksBtn.setStyle(HOVERED_BUTTON_STYLE);
         });
-        decksBtn.setOnMouseExited(event -> {
-            decksBtn.setStyle(IDLE_BUTTON_STYLE);
+
+        addBtn.setOnMouseClicked(event ->{
+            System.out.println("AddBtn pressed");
         });
 
-        addBtn.setOnMouseEntered(event ->{
-            decksBtn.setStyle(IDLE_BUTTON_STYLE);
-            addBtn.setStyle(HOVERED_BUTTON_STYLE);
-        });
-        addBtn.setOnMouseExited(event -> {
-            addBtn.setStyle(IDLE_BUTTON_STYLE);
-        });
+        browseBtn.setOnMousePressed(event -> {
 
-        browseBtn.setOnMouseEntered(event -> {
-            browseBtn.setStyle(HOVERED_BUTTON_STYLE);
         });
-        browseBtn.setOnMouseExited(event -> {
-            browseBtn.setStyle(IDLE_BUTTON_STYLE);
-        });
-
-        statsBtn.setOnMouseEntered(event -> {
-            statsBtn.setStyle(HOVERED_BUTTON_STYLE);
-        });
-        statsBtn.setOnMouseExited(event -> {
-            statsBtn.setStyle(IDLE_BUTTON_STYLE); 
-        });
+//        decksBtn.setOnMouseEntered(event -> {
+//            decksBtn.setStyle(HOVERED_BUTTON_STYLE);
+//        });
+//        decksBtn.setOnMousePressed(event ->{
+//            decksBtn.setStyle(HOVERED_BUTTON_STYLE);
+//        });
+//        decksBtn.setOnMouseExited(event -> {
+//            decksBtn.setStyle(IDLE_BUTTON_STYLE);
+//        });
+//
+//        addBtn.setOnMouseEntered(event ->{
+//            addBtn.setStyle(HOVERED_BUTTON_STYLE);
+//        });
+//        addBtn.setOnMouseClicked(event ->{
+//            System.out.println("AddBtn pressed");
+//        });
+//        addBtn.setOnMouseExited(event -> {
+//            addBtn.setStyle(IDLE_BUTTON_STYLE);
+//        });
+//
+//        browseBtn.setOnMouseEntered(event -> {
+//            browseBtn.setStyle(HOVERED_BUTTON_STYLE);
+//        });
+//        browseBtn.setOnMouseExited(event -> {
+//            browseBtn.setStyle(IDLE_BUTTON_STYLE);
+//        });
+//
+//        statsBtn.setOnMouseEntered(event -> {
+//            statsBtn.setStyle(HOVERED_BUTTON_STYLE);
+//        });
+//        statsBtn.setOnMouseExited(event -> {
+//            statsBtn.setStyle(IDLE_BUTTON_STYLE);
+//        });
     }
 }
